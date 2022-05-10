b8451b7677a426332f6a98396c081d85784189a0
hugocoding <mivck1012@gmail.com>
2022-05-10 18:00:15 +0800

    ^0.0.1

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
+"""
+***************PY_Attano Bot***************
+*************Author:hugocoding*************
+**************Version: 0.0.1***************
+"""
 import discord
 from discord.ext import commands
 
