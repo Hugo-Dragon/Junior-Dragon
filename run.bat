46bb57e0dbbddd754c32756decba5973d431823e
KL-AE2160 <88426249+KL-AE2160@users.noreply.github.com>
2022-05-12 20:32:57 +0800

    Fixed Bugs / Errors

--- run.bat
+++ run.bat
@@ -1,22 +1,30 @@
 @echo off
-echo Welcome to hugocoding, this batch file is edit by KL AE#2160
-echo What do you want to do
-choice /cs /c rie /m Please choose an option. r - Run the bot `ni - Install packages + Run the bot`ne - `Exit
-IF %ERRORLEVEL% === 2 goto x
-IF %ERRORLEVEL% === 1 goto y
-IF %ERRORLEVEL% === 3 exit
-:y
-echo 正在下載需要的套件 (Also installing optional packages)
-python -m pip install -r requirements.txt
-echo 正在開始運行機器人,請稍後...
+echo 1. Run the bot
+echo 2. Install packages
+echo 3. Exit
+choice /c 123 /n /m "Choice:"
+:: Note - list ERRORLEVELS in decreasing order
+IF ERRORLEVEL 3 GOTO optone
+IF ERRORLEVEL 2 GOTO install
+IF ERRORLEVEL 1 GOTO run
+
+:run
+echo Running Bot, Please wait...
 echo Python Version:
 python -version
 echo `n
 python app.py
 
-:x
-echo 正在開始運行機器人,請稍後...
-echo Python Version:
-python -version
-echo `n
-python app.py
+:install
+echo Installing Packages
+python -m pip install -r requirements.txt
+GOTO end
+
+:optone
+GOTO endx
+
+:endx
+exit
+
+:end
+pause
