ad6fa8fb9f4695aca90d6afb86a1ee704d7da8ce
hugocoding <mivck1012@gmail.com>
2022-05-13 17:33:03 +0800

    v0.0.2

--- cmds/game.py
+++ cmds/game.py
@@ -1,36 +1,38 @@
+from distutils.command.config import config
 import discord
 from discord.ext import commands
 from core.classes import Cog_Extension
 
-import asyncio, time, datetime, pytz, random
-from random import randint
+import json, asyncio, time, datetime, pytz, random
 
-import json
-with open("config.json", mode="r", encoding="utf8") as jfile:
-    conf = json.load(jfile)
+with open("config.json", mode="r", encoding="utf8") as config:
+    conf = json.load(config)
     
 class Game(Cog_Extension):
     @commands.command()
     async def gn(self, ctx):
         def check(m):
-            return m.author == ctx.author and m.channel == ctx.message.channel
-        number = random.randint(1,100)
+            return m.author == ctx.author and m.channel == ctx.message.channel 
+        number = random.randint(1,101) # Fixed Error (must add 1 to largest int)
+        sus = str(number)
         await ctx.send("Please guess a number from 1~100, you have 10 chances")
 
-        for i in range(0,10):
+        nowchance = 0
+        newline = '\n'
+        for i in range(0, 10):
             guess = await self.bot.wait_for("message", check=check)
-    
+            nowchance = nowchance+1
             if int(guess.content) > number:
-                await ctx.send("Too little")
+                await ctx.send(f"Uhh... The number is too little, try a bit more bigger{newline}Chances left: `{str(nowchance)}`")
             elif int(guess.content) < number:
-                await ctx.send("Too Large")
+                await ctx.send(f"Uhh... The number is too large, try a bit fewer {newline}Chances left: `{str(nowchance)}`")
             elif int(guess.content) == number:
-                await ctx.send("You finally finish this stupid game!!")
+                await ctx.send(f":tada: You finished this game!!! The number is {sus}! Congrats!")
             else:
-                await ctx.send("Please guess the number FROM 1 TO 100")
+                await ctx.send(f"Please guess the number between 1 and 100{newline}Chances left: `{str(nowchance)}`")
         else:
-            await ctx.send("Haha, you did NOT guess the correct number.")
-            await ctx.send(f"The Correct Number is:{number} ")
+            await ctx.send("Uhhh... You failed this game :sweat:")
+            await ctx.send(f"The Correct Number is: {sus}")
 
 
 
